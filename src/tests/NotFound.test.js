66d1dd07cb9d911fe87ea78de2e2ddc7ab68d785
Leandro Batista de Carvalho <leandrobcrv@gmail.com>
2022-12-04 19:55:00 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+describe('Testa se a página contém as informações corretas de Not Found', () => {
+  test('Testa se a página contém a messagem correta de Not Found', () => {
+    renderWithRouter(<NotFound />);
+    const notFound = screen.getByRole('heading', { name: /Page requested not found/i });
+    expect(notFound).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém a imagem correta', () => {
+    renderWithRouter(<NotFound />);
+    const imagem = screen.getByRole('img', { name: /Pikachu crying because the page requested was not found/i });
+    expect(imagem.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
