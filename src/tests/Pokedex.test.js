30079e9fce032d6031e9d8f0037ea5c40a64363e
Leandro Batista de Carvalho <leandrobcrv@gmail.com>
2022-12-04 20:15:50 -0300

    Requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testa se a página contém as informações corretas de Pokedex', () => {
+  test('Testa se a página contém um heading h2 com o texto Encountered Pokémon;', () => {
+    renderWithRouter(<App />);
+    const encounteredPokemon = screen.getByRole('heading', { name: /Encountered Pokémon/i });
+    expect(encounteredPokemon).toBeInTheDocument();
+  });
+
+  test('Testa se é exibido o próximo Pokémon da lista quando o botão Próximo Pokémon é clicado;', () => {
+    renderWithRouter(<App />);
+    const botaoPokedex = screen.getByRole('button', { name: /Próximo Pokémon/i });
+    expect(botaoPokedex).toBeInTheDocument();
+  });
+
+  test('Testa se a Pokédex tem os botões de reset;', () => {
+    renderWithRouter(<App />);
+    const todosPokemons = screen.getByRole('button', { name: /All/i });
+    expect(todosPokemons).toBeInTheDocument();
+  });
+});
