8cb7a7e21ab202068035e3475675296ae1517824
Leandro Batista de Carvalho <leandrobcrv@gmail.com>
2022-12-04 19:45:49 -0300

    Requisito 3

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemon from '../pages/FavoritePokemon';
+
+describe('Testa se a página contém as informações sobre os Pokemons Favoritos', () => {
+  test('Teste se é exibida na tela a mensagem caso a pessoa não tenha Pokémon favoritos;', () => {
+    renderWithRouter(<FavoritePokemon />);
+    const semFavorito = screen.getByText(/No favorite pokémon found/i);
+    expect(semFavorito).toBeInTheDocument();
+  });
+});
